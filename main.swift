a72356de66458de6eb0add74ec551bbcc0d3cdd8
s20171106577 <s20171106577@123.local>
2018-10-12 20:51:57 +0800

    test
    1jiadao100

---
+++ main.swift
@@ -0,0 +1,20 @@
+//
+//  main.swift
+//  test
+//
+//  Created by s20171106577 on 2018/10/12.
+//  Copyright © 2018年 s20171106577. All rights reserved.
+//
+
+import Foundation
+
+print("Hello, World!")
+
+var sum=0
+var i=1
+while i<101
+{
+    sum=sum+1
+    i=i+1
+}
+print("1加到100的结果：\(sum)")
